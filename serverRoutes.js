3994f79225e122b3973b7affcd019d5c5d4c6dac
LUCAS LIAO <lucas@11a0.com>
2023-09-21 09:23:59 +0800

    updated test

--- serverRoutes.js
+++ serverRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const isAuthenticated = require('./middleware/auth');
+const { isAuthenticated } = require('./middleware/auth');
 const serverController = require('./serverController');
 
 router.use(isAuthenticated);
--- test/test.js
+++ test/test.js
@@ -3,43 +3,130 @@ const axios = require('axios');
 // Define the base URL for your server
 const baseUrl = 'http://localhost:3000/api'; // Update with your actual server URL
 
-var clubId = null;
+// Admin JWT token, replace with your actual token
+const adminToken = 'your-admin-jwt-token';
+
+// Function to set JWT token in headers
+function setJwtHeaders() {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${adminToken}`;
+}
 
 // Function to test creating a new club
 async function testCreateClub() {
   try {
+    setJwtHeaders();
     const response = await axios.post(`${baseUrl}/clubs`, {
       name: 'Test Club',
     });
     console.log('Create Club Response:', response.data);
-    clubId = response.data._id;
+    const clubId = response.data._id; // Store the created club ID for future tests
+    return clubId;
   } catch (error) {
     console.error('Error creating club:', error.response.data);
+    return null;
   }
 }
 
-// Function to test updating club name
-async function testUpdateClubName(clubId) {
+// Function to test getting all clubs
+async function testGetAllClubs() {
   try {
-    const response = await axios.put(`${baseUrl}/clubs/${clubId}`, {
-      name: 'Updated Club HARIZZZ',
-    });
-    console.log('Update Club Name Response:', response.data);
+    setJwtHeaders();
+    const response = await axios.get(`${baseUrl}/clubs`);
+    console.log('Get All Clubs Response:', response.data);
   } catch (error) {
-    console.error('Error updating club name:', error.response.data);
+    console.error('Error getting all clubs:', error.response.data);
   }
 }
 
-// Function to test creating a new code
+// Function to test getting a club by ID
+async function testGetClubById(clubId) {
+  try {
+    setJwtHeaders();
+    const response = await axios.get(`${baseUrl}/clubs/${clubId}`);
+    console.log('Get Club by ID Response:', response.data);
+  } catch (error) {
+    console.error('Error getting club by ID:', error.response.data);
+  }
+}
+
+// Function to test updating a club
+async function testUpdateClub(clubId) {
+  try {
+    setJwtHeaders();
+    const response = await axios.put(`${baseUrl}/clubs/${clubId}`, {
+      name: 'Updated Club Name',
+    });
+    console.log('Update Club Response:', response.data);
+  } catch (error) {
+    console.error('Error updating club:', error.response.data);
+  }
+}
+
+// Function to test deleting a club
+async function testDeleteClub(clubId) {
+  try {
+    setJwtHeaders();
+    const response = await axios.delete(`${baseUrl}/clubs/${clubId}`);
+    console.log('Delete Club Response:', response.data);
+  } catch (error) {
+    console.error('Error deleting club:', error.response.data);
+  }
+}
+
+// Function to test joining a club
+async function testJoinClub(clubId) {
+  try {
+    setJwtHeaders();
+    const response = await axios.post(`${baseUrl}/clubs/${clubId}/join`);
+    console.log('Join Club Response:', response.data);
+  } catch (error) {
+    console.error('Error joining club:', error.response.data);
+  }
+}
+
+// Function to test getting profiles by user
+async function testGetProfilesByUser() {
+  try {
+    setJwtHeaders();
+    const response = await axios.get(`${baseUrl}/profiles`);
+    console.log('Get Profiles by User Response:', response.data);
+  } catch (error) {
+    console.error('Error getting profiles by user:', error.response.data);
+  }
+}
+
+// Function to test redeeming a code
+async function testRedeemCode(code) {
+  try {
+    setJwtHeaders();
+    const response = await axios.post(`${baseUrl}/redeem-code`, {
+      code: code,
+    });
+    console.log('Redeem Code Response:', response.data);
+  } catch (error) {
+    console.error('Error redeeming code:', error.response.data);
+  }
+}
+
+// Function to test deleting a profile
+async function testDeleteProfile(profileId) {
+  try {
+    setJwtHeaders();
+    const response = await axios.delete(`${baseUrl}/profiles/${profileId}`);
+    console.log('Delete Profile Response:', response.data);
+  } catch (error) {
+    console.error('Error deleting profile:', error.response.data);
+  }
+}
+
+// Function to test creating a code
 async function testCreateCode() {
   try {
+    setJwtHeaders();
     const response = await axios.post(`${baseUrl}/codes`, {
-      code: 'TEST123',
-      description: 'Test Code',
-      club: '6506d7c884891ef343c412bb', // Replace with the actual club ID
-      expires: '2023-12-31',
-      value: 100,
-      status: 'Active',
+      code: 'TESTCODE123',
+      expires: new Date(),
+      status: 'active',
     });
     console.log('Create Code Response:', response.data);
   } catch (error) {
@@ -47,132 +134,93 @@ async function testCreateCode() {
   }
 }
 
-// Function to test updating code description
-async function testUpdateCodeDescription(codeId) {
+// Function to test getting all codes
+async function testGetAllCodes() {
   try {
-    const response = await axios.put(`${baseUrl}/codes/${codeId}/description`, {
-      description: 'Updated Code Description',
-    });
-    console.log('Update Code Description Response:', response.data);
+    setJwtHeaders();
+    const response = await axios.get(`${baseUrl}/codes`);
+    console.log('Get All Codes Response:', response.data);
   } catch (error) {
-    console.error('Error updating code description:', error.response.data);
+    console.error('Error getting all codes:', error.response.data);
   }
 }
 
-// Function to test updating code expiry
-async function testUpdateCodeExpiry(codeId) {
+// Function to test updating a code
+async function testUpdateCode(codeId) {
   try {
-    const response = await axios.put(`${baseUrl}/codes/${codeId}/expiry`, {
-      expires: '2024-12-31',
+    setJwtHeaders();
+    const response = await axios.put(`${baseUrl}/codes/${codeId}`, {
+      status: 'inactive',
     });
-    console.log('Update Code Expiry Response:', response.data);
+    console.log('Update Code Response:', response.data);
   } catch (error) {
-    console.error('Error updating code expiry:', error.response.data);
+    console.error('Error updating code:', error.response.data);
   }
 }
 
-// Function to test updating code status
-async function testUpdateCodeStatus(codeId) {
+// Function to test deleting a code
+async function testDeleteCode(codeId) {
   try {
-    const response = await axios.put(`${baseUrl}/codes/${codeId}/status`, {
-      status: 'Inactive',
-    });
-    console.log('Update Code Status Response:', response.data);
+    setJwtHeaders();
+    const response = await axios.delete(`${baseUrl}/codes/${codeId}`);
+    console.log('Delete Code Response:', response.data);
   } catch (error) {
-    console.error('Error updating code status:', error.response.data);
+    console.error('Error deleting code:', error.response.data);
   }
 }
 
-// Function to test creating a new fan
-async function testCreateFan() {
+// Function to test getting all fans
+async function testGetAllFans() {
   try {
-    const response = await axios.post(`${baseUrl}/fans`, {
-      fanMobile: {
-        countryCode: '+1',
-        number: '1234567890',
-      },
-      fanEmail: 'test@example.com',
-    });
-    console.log('Create Fan Response:', response.data);
+    setJwtHeaders();
+    const response = await axios.get(`${baseUrl}/fans`);
+    console.log('Get All Fans Response:', response.data);
   } catch (error) {
-    console.error('Error creating fan:', error.response.data);
+    console.error('Error getting all fans:', error.response.data);
   }
 }
 
-// Function to test updating fan mobile
-async function testUpdateFanMobile(fanId) {
+// Function to test getting a fan by ID
+async function testGetFanById(fanId) {
   try {
-    const response = await axios.put(`${baseUrl}/fans/${fanId}/mobile`, {
-      countryCode: '+1',
-      number: '9876543210',
-    });
-    console.log('Update Fan Mobile Response:', response.data);
+    setJwtHeaders();
+    const response = await axios.get(`${baseUrl}/fans/${fanId}`);
+    console.log('Get Fan by ID Response:', response.data);
   } catch (error) {
-    console.error('Error updating fan mobile:', error.response.data);
+    console.error('Error getting fan by ID:', error.response.data);
   }
 }
 
-// Function to test updating fan email
-async function testUpdateFanEmail(fanId) {
+// Function to test getting the current user's fan profile
+async function testGetMyself() {
   try {
-    const response = await axios.put(`${baseUrl}/fans/${fanId}/email`, {
-      fanEmail: 'updated@example.com',
-    });
-    console.log('Update Fan Email Response:', response.data);
+    setJwtHeaders();
+    const response = await axios.get(`${baseUrl}/myself`);
+    console.log('Get Myself Response:', response.data);
   } catch (error) {
-    console.error('Error updating fan email:', error.response.data);
+    console.error('Error getting myself:', error.response.data);
   }
 }
 
-// Function to test setting fan rtId
-async function testSetFanRtId(fanId) {
+// Call the main testing function to start the flow
+async function main() {
   try {
-    const response = await axios.put(`${baseUrl}/fans/${fanId}/rtid`, {
-      rtId: 'RT12345',
-    });
-    console.log('Set Fan RTID Response:', response.data);
+    const clubId = await testCreateClub();
+    await testGetAllClubs();
+    if (clubId) {
+      await testGetClubById(clubId);
+      await testUpdateClub(clubId);
+      await testDeleteClub(clubId);
+      await testJoinClub(clubId);
+    }
+    await testGetProfilesByUser();
+    const code = 'TESTCODE123'; // Replace with a valid code
+    await testRedeemCode(code);
+
+    await testDeleteProfile('profileId123'); // Replace with a valid profile ID
   } catch (error) {
-    console.error('Error setting fan RTID:', error.response.data);
+    console.error('Error:', error.response.data);
   }
 }
 
-// Function to test creating a new profile
-async function testCreateProfile() {
-  try {
-    const response = await axios.post(`${baseUrl}/profiles`, {
-      fanId: 'fanId', // Replace with the actual fan ID
-      club: 'clubId', // Replace with the actual club ID
-    });
-    console.log('Create Profile Response:', response.data);
-  } catch (error) {
-    console.error('Error creating profile:', error.response.data);
-  }
-}
-
-// Function to test activating a code with an identifier
-async function testActivateCode(identifier, code) {
-  try {
-    const response = await axios.post(`${baseUrl}/activate-code`, {
-      identifier,
-      code,
-    });
-    console.log('Activate Code Response:', response.data);
-  } catch (error) {
-    console.error('Error activating code:', error.response.data);
-  }
-}
-
-// Example usage of the test functions
-// testCreateClub();
-// testUpdateClubName("6506d7c884891ef343c412bb");
-testCreateCode();
-// testUpdateCodeDescription();
-// testUpdateCodeExpiry();
-// testUpdateCodeStatus();
-// testCreateFan();
-// testUpdateFanMobile();
-// testUpdateFanEmail();
-// testSetFanRtId();
-// testCreateProfile();
-// testActivateCode();
-
+main();
